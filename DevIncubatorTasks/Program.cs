6b9f426de9a44aff3d9dd35d42823be5f1c7b4a2
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2020-12-30 23:18:20 +0300

    Complete b part of Task1

--- DevIncubatorTasks/Program.cs
+++ DevIncubatorTasks/Program.cs
@@ -6,6 +6,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //a)
             //acquaintance with world
             Console.WriteLine("Hello World!");
             
@@ -14,12 +15,31 @@ namespace Task1
             int age = 19;
            
             // request data
+            Console.WriteLine("Enter your name");
             name = Console.ReadLine();
+            
+            Console.WriteLine("Enter your age");
             age = int.Parse(Console.ReadLine());
 
             //display data about user
             Console.WriteLine($"My name is {name}. My age is {age}");
             
+            //b)
+            //declare 3 variables
+            double realNumber;
+            byte smallNumber;
+            char symbol;
+            
+            //request data for numbers and symbol
+            Console.WriteLine("Enter real number");
+            realNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter small number");
+            smallNumber = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter symbol");
+            symbol = char.Parse(Console.ReadLine());
+
+            //display data
+            Console.WriteLine($"{realNumber} {smallNumber} {symbol}");
         }
     }
 }
