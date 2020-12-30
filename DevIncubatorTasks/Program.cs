a4aeca4d60bd209cbb185ea7b3694af34fef4033
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2020-12-30 23:11:45 +0300

    Complete a part of Task1

--- DevIncubatorTasks/Program.cs
+++ DevIncubatorTasks/Program.cs
@@ -6,7 +6,20 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //acquaintance with world
             Console.WriteLine("Hello World!");
+            
+            //declare 2 variables
+            string name;
+            int age = 19;
+           
+            // request data
+            name = Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+
+            //display data about user
+            Console.WriteLine($"My name is {name}. My age is {age}");
+            
         }
     }
 }
