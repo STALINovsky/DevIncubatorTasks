a70ad8cca500928674c9d4f05dca27fdbba597b0
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-01 01:00:52 +0300

    Complete Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Swap<T>(ref T left, ref T right)
+        {
+            (left, right) = (right, left);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b");
+            int b = int.Parse(Console.ReadLine());
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}
