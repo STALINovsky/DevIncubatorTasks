2c295cc46c6428270b1d947d4b3386a7ee6e2e25
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2020-12-31 21:07:07 +0300

    Complete Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Task7
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<int> ReadAssessments()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type assessments, to stop just press Enter");
+            var assessments = new List<int>();
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                if (userInput == string.Empty)
+                {
+                    return assessments;
+                }
+                else
+                {
+                    assessments.Add(int.Parse(userInput));
+                }
+            }
+        }
+
+        static double CalculateRating(params int[] assessments)
+        {
+            var maxAssessment = assessments.Max();
+            var minAssessment = assessments.Min();
+
+            var listOfAssessments = assessments.ToList();
+
+            listOfAssessments.Remove(maxAssessment);
+            listOfAssessments.Remove(minAssessment);
+
+            return listOfAssessments.Average();
+        }
+
+        static void Main(string[] args)
+        { 
+            var assessments = ReadAssessments();
+            double average = CalculateRating(assessments.ToArray());
+
+            Console.WriteLine(average);
         }
     }
 }
