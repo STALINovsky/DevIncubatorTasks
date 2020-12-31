f607bf93589c13b590d5b8a17de7fdbf28af945c
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-01 01:16:45 +0300

    Complete Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Text;
 
 namespace Task11
 {
     class Program
     {
+        static string CreateFullName(string firstName = "", string middleName = "", string lastName = "")
+        {
+            var stringBuilder = new StringBuilder();
+            var separator = " ";
+            return string.Concat(firstName, separator, middleName, separator, lastName);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(CreateFullName("Alex"));
+            Console.WriteLine(CreateFullName("Fredd","McGregor"));
+            Console.WriteLine(CreateFullName(firstName: "Dmitry", lastName: "Ovchinnikov"));
         }
     }
 }
