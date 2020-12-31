86ef5a07f0261dbe23f1c2857b354644c113ae0d
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-01 01:29:03 +0300

    add comments to Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,15 +5,24 @@ namespace Task11
 {
     class Program
     {
+        /// <summary>
+        /// Create full name by firstname, middleName, lastName
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="middleName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
         static string CreateFullName(string firstName = "", string middleName = "", string lastName = "")
         {
             var stringBuilder = new StringBuilder();
             var separator = " ";
+            //using string concat to make it faster
             return string.Concat(firstName, separator, middleName, separator, lastName);
         }
 
         static void Main(string[] args)
         {
+            //testing result of method
             Console.WriteLine(CreateFullName("Alex"));
             Console.WriteLine(CreateFullName("Fredd","McGregor"));
             Console.WriteLine(CreateFullName(firstName: "Dmitry", lastName: "Ovchinnikov"));
