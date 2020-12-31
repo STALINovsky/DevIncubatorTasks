f4b2ad873b2bd010578ac1a4aed1710f9bef60f7
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2020-12-31 08:03:13 +0300

    Complete Task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Task5
 {
     class Program
     {
+        /// <summary>
+        /// Method return List of even numbers [min,max]
+        /// </summary>
+        /// <param name="min">start of range</param>
+        /// <param name="max">end of range</param>
+        /// <returns>even numbers between [min,max]</returns>
+        static List<int> GetRangeOfEvenNumbers(int min, int max)
+        {
+            var evenNumbers = new List<int>();
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 != 0) // if number isn't even, then start new iteration
+                {
+                    continue;
+                }
+                evenNumbers.Add(i);
+            }
+
+            return evenNumbers;
+        }
+
+        /// <summary>
+        /// Show sequence of numbers on console
+        /// </summary>
+        /// <param name="numbers"></param>
+        static void PrintNumbers(IEnumerable<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //define and assign limits of even numbers
+            Console.WriteLine("Enter first Limit");
+            int firstLimit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second Limit");
+            int secondLimit = int.Parse(Console.ReadLine());
+
+            //guarantees the correct work of the method
+            var evenNumbers = firstLimit > secondLimit
+                ? GetRangeOfEvenNumbers(secondLimit, firstLimit)
+                : GetRangeOfEvenNumbers(firstLimit, secondLimit);
+            
+            //output
+            PrintNumbers(evenNumbers);
         }
     }
 }
