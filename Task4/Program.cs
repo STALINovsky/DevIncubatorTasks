cd1f19b800709c916f48bac8ade4fed4b2a4e0e2
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2020-12-31 07:19:24 +0300

    Complete Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Task4
 {
     class Program
     {
+        static int ReadNumber()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Enter number of Day");
+                var numberOfDay = ReadNumber();
+
+                string message;
+                switch (numberOfDay)
+                {
+                    case 1:
+                        message = "Monday";
+                        break;
+                    case 2:
+                        message = "Tuesday";
+                        break;
+                    case 3:
+                        message = "Wednesday";
+                        break;
+                    case 4:
+                        message = "Thursday";
+                        break;
+                    case 5:
+                        message = "friday";
+                        break;
+                    case 6:
+                        message = "Saturday";
+                        break;
+                    case 7:
+                        message = "Sunday";
+                        break;
+                    default:
+                        return;
+                }
+
+                Console.WriteLine(message);
+            }
+            
         }
     }
 }
