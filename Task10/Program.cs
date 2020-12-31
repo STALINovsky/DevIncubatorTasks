bf10e7ea5c16789170ceea0c385a6d62000b0004
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-01 01:26:35 +0300

    Add comments to Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,6 +4,12 @@ namespace Task10
 {
     class Program
     {
+        /// <summary>
+        /// Swap two elements
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
         static void Swap<T>(ref T left, ref T right)
         {
             (left, right) = (right, left);
@@ -11,13 +17,14 @@ namespace Task10
 
         static void Main(string[] args)
         {
+            //Assign test variables 
             Console.WriteLine("enter a");
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter b");
             int b = int.Parse(Console.ReadLine());
 
             Swap(ref a, ref b);
-
+            //Show result of method
             Console.WriteLine($"a = {a}, b = {b}");
         }
     }
