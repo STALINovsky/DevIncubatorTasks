e7d26be94a8f6811222d6089a5c50dbeb363f35d
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-01 01:23:17 +0300

    Complete Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -4,9 +4,19 @@ namespace Task12
 {
     class Program
     {
+        static void Print(params object[] objects)
+        {
+            foreach (var obj in objects)
+            {
+                var objectType = obj.GetType();
+                Console.Write($"{objectType} ");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Print(10, "Hello", 10.0);
+            Print();
         }
     }
 }
