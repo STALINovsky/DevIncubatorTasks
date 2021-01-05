2e254609dfb676c4c603b2f18cbc9ecb35ff8b4f
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-05 18:58:15 +0300

    Add comments to Task 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -6,17 +6,29 @@ namespace Task29
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Method that fill list [1, count]
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        static LinkedList<int> InitCircle(int count)
         {
             LinkedList<int> circle = new LinkedList<int>();
-            var circleCount = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= circleCount; i++)
+            for (int i = 1; i <= count; i++)
             {
                 circle.AddLast(i);
             }
 
+            return circle;
+        }
 
+        static void Main(string[] args)
+        {
+            //create circle
+            var circleCount = int.Parse(Console.ReadLine());
+            var circle = InitCircle(circleCount);
+            
+            //loop that remove every even element while circle count isn't 1
             bool isElementEven = false;
             LinkedListNode<int> currentElement = circle.First;
             while (circle.Count != 1)
