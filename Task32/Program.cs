25812809b9de45ce31fcdd4e0865e633e5055a66
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2021-01-13 11:41:42 +0300

    Complete task 32

---
+++ Task32/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal, decimal> mathOperation;
+
+            Console.WriteLine("Enter first Number");
+            var firstNumber = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter operation");
+            var operatorSymbol = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            var secondNumber = decimal.Parse(Console.ReadLine());
+
+            mathOperation = operatorSymbol switch
+            {
+                "+" => (left, right) => left + right,
+                "-" => (left, right) => left - right,
+                "*" => (left, right) => left * right,
+                "/" => (left, right) => left / right,
+                _ => throw new ArgumentOutOfRangeException(nameof(operatorSymbol), operatorSymbol, "Invalid type of operation")
+            };
+
+            Console.WriteLine($"Result of operation: {mathOperation(firstNumber, secondNumber)}");
+
+        }
+    }
+}
